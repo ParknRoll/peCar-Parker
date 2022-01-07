7e21d383f6a439ed7b990d44799cd4cc9eb67744
Dexter <dextersimch@gmail.com>
2022-01-07 15:28:00 +0800

    display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Display from "./components/Display";
 import { fetchAvailability, fetchInformation } from "./Functions/Fetch";
 
 const App = () => {
@@ -14,11 +15,10 @@ const App = () => {
     };
   }, []);
 
-  fetchInformation();
-
   return (
     <div className="App">
       <header className="App-header">test</header>
+      <Display carpark={state} />
     </div>
   );
 };
