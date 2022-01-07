bf6011fb4dd2a11b2c469cefd8fae73b99c70430
Dexter <dextersimch@gmail.com>
2022-01-07 11:21:11 +0800

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const fetchData = () => {
+    return fetch("https://api.data.gov.sg/v1/transport/carpark-availability")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">test</header>
     </div>
   );
-}
+};
 
 export default App;
