8b7a3cd674a9e6e1ee8da1cec91af1555648796a
Jin Wei <seetohjinwei@gmail.com>
2022-01-07 11:29:38 +0800

    added blank DisplaySearch and Search components

---
+++ src/components/DisplaySearch.tsx
@@ -0,0 +1,5 @@
+const DisplaySearch = () => {
+    return <h1>This is the display of the information.</h1>;
+}
+
+export default DisplaySearch;
---
+++ src/components/Search.tsx
@@ -0,0 +1,5 @@
+const Search = () => {
+    return <h1>This is the search box.</h1>;
+}
+
+export default Search;
