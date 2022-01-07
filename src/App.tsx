6aa0fab859a0c099ccc943c93c4f65aefa7883df
Jin Wei <seetohjinwei@gmail.com>
2022-01-07 17:30:55 +0800

    fixed api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { stringify } from "querystring";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Display from "./components/Display";
 import ICarpark from "./components/Interfaces/InterfaceCarpark";
-import { fetchCarparks } from "./Functions/Fetch";
+import { fetchCarparkAvailability, fetchCarparks } from "./Functions/Fetch";
 
 const App = () => {
-  const [carparks, setCarparks] = useState<ICarpark[]>([]);
-  useEffect(() => fetchCarparks("ALJUNIED", setCarparks), []); // temporary, we will have to call fetchData with name that user has inputted.
+  const [filteredCarparks, setFilteredCarparks] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchCarparkAvailability("aljunied", setFilteredCarparks);
+  }, []); // temporary, we will have to call fetchData with name that user has inputted.
 
   return (
     <div className="App">
       <header className="App-header">test</header>
-      <Display carparks={carparks} />;
+      <Display carparks={filteredCarparks} />;
     </div>
   );
 };
